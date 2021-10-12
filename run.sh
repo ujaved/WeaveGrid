85ce08ab9cf0944dafe2167182019ce64d2d7bbd
Umar Javed <ujaved@amazon.com>
2021-10-12 02:02:52 -0500

    Mounting the host directiry in the container

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ echo "base directory: $1"
 echo "building docker image"
 docker build -t wg-image .
  
-container_id=`docker run --rm -d -p 5000:5000 -e BASE_DIR=$1 wg-image`
+container_id=`docker run --rm -d -p 5000:5000 -v $1:$1 -e BASE_DIR=$1 wg-image`
 echo "Launched API container on http://localhost:5000 with container id: $container_id"
